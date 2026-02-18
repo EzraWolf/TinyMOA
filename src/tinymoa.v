472407b14245927e4b7466cf6703da5e20ea0114
Ezra Wolf <wolfez@mail.uc.edu>
2026-02-18 01:08:18 -0500

    iCE40 Example

--- src/tinymoa.v
+++ src/tinymoa.v
@@ -1,1 +1,12 @@
-// WIP
+// Testing Alchitry Cu (iCE40-HX8K) compilation using the IIC-OSIC toolchain
+
+module top (
+    input  clk,
+    output reg [7:0] led
+);
+    reg [25:0] counter;
+    always @(posedge clk) begin
+        counter <= counter + 1;
+        led <= counter[25:18];
+    end
+endmodule
