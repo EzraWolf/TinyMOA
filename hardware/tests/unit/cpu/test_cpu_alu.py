5547f3b2f020ba11d0e8e30259de9487036c8a57
Ezra Wolf <wolfez@mail.uc.edu>
2026-06-08 12:22:42 -0700

    add AluOp enum

--- hardware/tests/unit/cpu/test_cpu_alu.py
+++ hardware/tests/unit/cpu/test_cpu_alu.py
@@ -1,10 +1,29 @@
 import cocotb
 import random
+from enum import Enum
 from cocotb.triggers import Timer
 
 
-async def _alu(dut, i_op, i_a, i_b):
-    dut.i_op.value = i_op
+class AluOp(Enum):
+    ADD = 0b0000
+    ADDW = 0b0001
+    SUB = 0b0010
+    SUBW = 0b0011
+    OR = 0b0100
+    AND = 0b0101
+    XOR = 0b0110
+    SLT = 0b0111
+    SLTU = 0b1000
+    SLL = 0b1001
+    SLLW = 0b1010
+    SRL = 0b1011
+    SRLW = 0b1100
+    SRA = 0b1101
+    SRAW = 0b1110
+
+
+async def _alu(dut, i_op: AluOp, i_a, i_b):
+    dut.i_op.value = i_op.value
     dut.i_a.value = i_a
     dut.i_b.value = i_b
     await Timer(1, "ns")
@@ -18,7 +37,7 @@ async def add_basic(dut):
         a = random.randint(0, max_size)
         b = random.randint(0, max_size)
         expected = (a + b) & max_size
-        actual = await _alu(dut, 0b0000, a, b)
+        actual = await _alu(dut, AluOp.ADD, a, b)
         assert expected == actual
 
 
@@ -32,7 +51,7 @@ async def addw_basic(dut):
         expected = (a + b) & max_size
         if expected >> 31:
             expected |= sign_ext
-        actual = await _alu(dut, 0b0001, a, b)
+        actual = await _alu(dut, AluOp.ADDW, a, b)
         assert expected == actual
 
 
@@ -43,7 +62,7 @@ async def sub_basic(dut):
         a = random.randint(0, max_size)
         b = random.randint(0, max_size)
         expected = (a - b) & max_size
-        actual = await _alu(dut, 0b0010, a, b)
+        actual = await _alu(dut, AluOp.SUB, a, b)
         assert expected == actual
 
 
@@ -57,5 +76,5 @@ async def subw_basic(dut):
         expected = (a - b) & max_size
         if expected >> 31:
             expected |= sign_ext
-        actual = await _alu(dut, 0b0011, a, b)
+        actual = await _alu(dut, AluOp.SUBW, a, b)
         assert expected == actual
