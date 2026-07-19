098c12dac0a3dbc6fb578cfe40736f12e110372c
Ezra Wolf <wolfez@mail.uc.edu>
2026-07-19 04:54:52 -0700

    fix checker

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,5 +3,13 @@ set -eu
 
 cd "$(dirname "$0")/.."
 
+check_log="$(mktemp)"
+trap 'rm -f "$check_log"' EXIT
+
+check_status=0
+veryl check >"$check_log" 2>&1 || check_status=$?
+sed '/^\[INFO \]/d' "$check_log"
+[ "$check_status" -eq 0 ] || exit "$check_status"
+
 veryl build --quiet
 uv run pytest --no-header --tb=short -q -n auto
