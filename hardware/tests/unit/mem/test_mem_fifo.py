05f597f2a4685028dcda53ad241e0cc2e89b51e9
Ezra Wolf <wolfez@mail.uc.edu>
2026-07-18 17:08:28 -0700

    clean up fifo

--- hardware/tests/unit/mem/test_mem_fifo.py
+++ hardware/tests/unit/mem/test_mem_fifo.py
@@ -1,10 +1,11 @@
 import os
-import random
 import pytest
 import cocotb
+from random import randint
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles, Timer
-from ...runner import run
+from cocotb.triggers import FallingEdge, Timer
+
+from tests.runner import run
 
 
 WIDTH = int(os.environ.get("WIDTH", 64))
@@ -21,38 +22,26 @@ async def setup(dut):
     dut.i_data.value = 0
     dut.i_ren.value = 0
 
-    await ClockCycles(dut.clk, 1)
+    await FallingEdge(dut.clk)
     dut.rst.value = 1
+
+    await _check(dut)
+
+
+async def _check(dut, rst=1, i_wen=0, i_data=0, o_full=0, i_ren=0, o_data=0, o_empty=1):
     await Timer(1, unit="ns")
-
-    await _check_dut(dut)
-
-
-async def _check_dut(
-    dut, rst=1, i_wen=0, i_data=0, o_full=0, i_ren=0, o_data=0, o_empty=1
-):
-    await Timer(1, unit="ns")
-    assert dut.rst.value == rst, f"rst expected {rst}, got {dut.rst.value}"
-    assert dut.i_wen.value == i_wen, f"i_wen expected {i_wen}, got {dut.i_wen.value}"
-    assert dut.i_data.value == i_data, (
-        f"i_data expected {i_data}, got {dut.i_data.value}"
-    )
-    assert dut.o_full.value == o_full, (
-        f"o_full expected {o_full}, got {dut.o_full.value}"
-    )
-    assert dut.i_ren.value == i_ren, f"i_ren expected {i_ren}, got {dut.i_ren.value}"
-    assert dut.o_data.value == o_data, (
-        f"o_data expected {o_data}, got {dut.o_data.value}"
-    )
-    assert dut.o_empty.value == o_empty, (
-        f"o_empty expected {o_empty}, got {dut.o_empty.value}"
-    )
+    assert dut.rst.value == rst
+    assert dut.i_wen.value == i_wen
+    assert dut.i_data.value == i_data
+    assert dut.o_full.value == o_full
+    assert dut.i_ren.value == i_ren
+    assert dut.o_data.value == o_data
+    assert dut.o_empty.value == o_empty
 
 
 @cocotb.test()
-async def setup_is_proper(dut):
+async def proper_io_width(dut):
     await setup(dut)
-    await _check_dut(dut)
 
     # TODO: figure out how to test param DEPTH
     assert WIDTH == len(dut.i_data)
@@ -64,68 +53,75 @@ async def setup_is_proper(dut):
 # async def write_deasserts_empty(dut, DATA):
 async def write_deasserts_empty(dut):
     await setup(dut)
+
     dut.i_wen.value = 1
     dut.i_data.value = 0xDEAD
-    await ClockCycles(dut.clk, 1)
-    await _check_dut(dut, i_wen=1, i_data=0xDEAD, o_empty=0)
+    await FallingEdge(dut.clk)
+    await _check(dut, i_wen=1, i_data=0xDEAD, o_empty=0)
 
 
 @cocotb.test()
 async def fill_full(dut):
     await setup(dut)
+
     for _ in range(DEPTH):
         dut.i_wen.value = 1
-        dut.i_data.value = random.randint(0, XLEN_MASK)
-        await ClockCycles(dut.clk, 1)
+        dut.i_data.value = randint(0, XLEN_MASK)
+        await FallingEdge(dut.clk)
+
     dut.i_wen.value = 0
     dut.i_data.value = 0
-    await _check_dut(dut, o_full=1, o_empty=0)
+    await _check(dut, o_full=1, o_empty=0)
 
 
 @cocotb.test()
 async def write_on_full(dut):
     await setup(dut)
-    first = random.randint(0, XLEN_MASK)
+
+    first = randint(0, XLEN_MASK)
     dut.i_wen.value = 1
     dut.i_data.value = first
-    await ClockCycles(dut.clk, 1)
+    await FallingEdge(dut.clk)
     for _ in range(DEPTH - 1):
-        dut.i_data.value = random.randint(0, XLEN_MASK)
-        await ClockCycles(dut.clk, 1)
+        dut.i_data.value = randint(0, XLEN_MASK)
+        await FallingEdge(dut.clk)
+
     dut.i_wen.value = 0
     dut.i_data.value = 0
     dut.i_ren.value = 1
-    await ClockCycles(dut.clk, 1)
-    await _check_dut(dut, i_ren=1, o_data=first, o_empty=0)
+    await FallingEdge(dut.clk)
+    await _check(dut, i_ren=1, o_data=first, o_empty=0)
 
 
 @cocotb.test()
 async def read_empty(dut):
     await setup(dut)
+
     dut.i_ren.value = 1
-    await ClockCycles(dut.clk, 1)
-    await _check_dut(dut, i_ren=1, o_empty=1, o_data=0)
+    await FallingEdge(dut.clk)
+    await _check(dut, i_ren=1, o_empty=1, o_data=0)
 
 
 @cocotb.test()
 async def read_to_empty(dut):
     await setup(dut)
+
     vals = []
     dut.i_wen.value = 1
     for _ in range(DEPTH):
-        v = random.randint(0, XLEN_MASK)
+        v = randint(0, XLEN_MASK)
         vals.append(v)
         dut.i_data.value = v
-        await ClockCycles(dut.clk, 1)
+        await FallingEdge(dut.clk)
+
     dut.i_wen.value = 0
     dut.i_ren.value = 1
     dut.i_data.value = 0
     for v in vals:
         dut.i_ren.value = 1
-        await ClockCycles(dut.clk, 1)
-        await Timer(1, "ns")
+        await FallingEdge(dut.clk)
         assert dut.o_data.value == v, f"expected {hex(v)} got {hex(dut.o_data.value)}"
-    await _check_dut(dut, i_ren=1, o_empty=1, o_data=vals[-1])
+    await _check(dut, i_ren=1, o_empty=1, o_data=vals[-1])
 
 
 @pytest.mark.parametrize(
