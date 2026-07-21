e16b40b08b71ad9351c090cb0651fe3d05ce43ab
Ezra Wolf <wolfez@mail.uc.edu>
2026-07-21 01:01:01 -0700

    optimize ALU LUT/FF count

--- hardware/tests/common/cpu_types.py
+++ hardware/tests/common/cpu_types.py
@@ -3,20 +3,20 @@ from enum import IntEnum
 
 class AluOp(IntEnum):
     ADD = 0b0000
-    ADDW = 0b0001
-    SUB = 0b0010
-    SUBW = 0b0011
-    OR = 0b0100
+    SUB = 0b0001
+    SLL = 0b0010
+    SRL = 0b0011
+    SRA = 0b0100
     AND = 0b0101
-    XOR = 0b0110
-    SLT = 0b0111
-    SLTU = 0b1000
-    SLL = 0b1001
+    OR = 0b0110
+    XOR = 0b0111
+    ADDW = 0b1000
+    SUBW = 0b1001
     SLLW = 0b1010
-    SRL = 0b1011
-    SRLW = 0b1100
-    SRA = 0b1101
-    SRAW = 0b1110
+    SRLW = 0b1011
+    SRAW = 0b1100
+    SLT = 0b1101
+    SLTU = 0b1110
 
 
 class FuSrc1(IntEnum):
